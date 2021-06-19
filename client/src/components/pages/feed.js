bdfa96a743f90ceb51e5fc8387169c09bb7eb3ff
Ayushman <51037059+Dark0017@users.noreply.github.com>
2021-06-19 15:37:44 +0530

    added validaton to form input

--- client/src/components/pages/feed.js
+++ client/src/components/pages/feed.js
@@ -4,7 +4,7 @@ import Pagination from "../pagination";
 
 const Feed = () => {
   return (
-    <div className="flex flex-col h-screen w-2/3 mx-auto gap-y-3 p-3    items-center">
+    <div className="flex flex-col h-screen w-2/3 mx-auto gap-y-3 p-3 items-center">
       Items
       <ItemCard />
       <Pagination />
--- client/src/components/pages/form.js
+++ client/src/components/pages/form.js
@@ -1,28 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import Joi from "joi-browser";
+
 const Form = () => {
+  const [movie, setMovie] = useState({ name: "", year: "", language: "" });
+
+  const movieSchema = {
+    name: Joi.string().required(),
+    year: Joi.string().length(4).required(),
+    language: Joi.string().required(),
+  };
+
+  const getYears = (startYear) => {
+    var currentYear = new Date().getFullYear(),
+      years = [];
+    startYear = startYear || 1888;
+    while (startYear <= currentYear) {
+      years.push(currentYear--);
+    }
+    return years;
+  };
+
+  const years = getYears();
+
+  const validateForm = () => {
+    const res = Joi.validate(movie, movieSchema);
+    if (res.error) {
+      console.log(res.error.details[0].message);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validateForm();
+    console.log("current movie: ", movie);
+  };
   return (
-    <div className="container mx-auto py-20">
-      <div className="flex flex-col bg-green-100 w-3/5 gap-y-3 mx-auto p-4">
+    <form className="container py-20 mx-auto" onSubmit={handleSubmit}>
+      <div className="flex flex-col w-3/5 p-4 mx-auto bg-green-100 gap-y-3">
         <div className="flex flex-row flex-grow gap-x-4">
           <div className="flex flex-col w-2/3 gap-y-5">
             <div className="flex flex-row w-full gap-x-3">
-              <input placeholder="Name" className="w-2/4 h-20 p-1" />
-              <input placeholder="Year" className="w-2/4 h-20 p-1" />
+              <input
+                id="nameInput"
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={movie.name}
+                onChange={(e) =>
+                  setMovie({ ...movie, name: e.currentTarget.value })
+                }
+                className="w-2/4 h-20 p-1"
+              />
+              <input
+                id="yearInput"
+                type="text"
+                name="year"
+                placeholder="Year"
+                value={movie.year}
+                onChange={(e) =>
+                  setMovie({ ...movie, year: e.currentTarget.value })
+                }
+                list="years"
+                className="w-2/4 h-20 p-1"
+              />
+              <datalist id="years" className="bg-red-600">
+                {years.map((year) => (
+                  <option>{year}</option>
+                ))}
+              </datalist>
             </div>
-            <div className="w-full bg-yellow-100 h-20 p-1">Language</div>
+            <div>
+              <input
+                id="languageInput"
+                type="text"
+                name="language"
+                value={movie.language}
+                onChange={(e) =>
+                  setMovie({ ...movie, language: e.currentTarget.value })
+                }
+                className="w-full h-20 p-1 bg-yellow-100"
+                placeholder="Language"
+              />
+            </div>
           </div>
-          <input type="file" className="w-1/3 p-2 bg-red-100"/>
+          <input id="thumbNail" type="file" className="w-1/3 p-2 bg-red-100" />
         </div>
         <input
+          id="videoInput"
           type="file"
           placeholder="Upload File"
-          className=" bg-pink-100 h-24 p-5 mt-7 mb-3"
+          className="h-24 p-5 mb-3 bg-pink-100 mt-7"
         />
       </div>
-      <div className="w-3/5 gap-y-3 mx-auto py-4">
-        <button className="bg-blue-300 w-full h-16">Done</button>
+      <div className="w-3/5 py-4 mx-auto gap-y-3">
+        <button type="submit" className="w-full h-16 bg-blue-300">
+          Done
+        </button>
       </div>
-    </div>
+    </form>
   );
 };
 
