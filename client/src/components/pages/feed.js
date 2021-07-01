9974fe7bfe70cfb79360a3c94b64bc5f6a869a0b
Ayushman <51037059+Dark0017@users.noreply.github.com>
2021-07-01 13:41:30 +0530

    modifying the feed single item

--- client/src/components/pages/feed.js
+++ client/src/components/pages/feed.js
@@ -4,7 +4,7 @@ import ItemCard from "../itemCard";
 
 const Feed = () => {
   return (
-    <div className="flex flex-col items-center h-screen p-3 mx-auto w-6/7 gap-y-3">
+    <div className="flex flex-col items-center h-screen p-3 mx-auto bg-red-300 w-6/7 gap-y-3">
       Items
       <ItemCard />
       {/* <Pagination /> */}
